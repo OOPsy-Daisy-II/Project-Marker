1a0d995be1e5c51ca737eb988348e9fc21c62aef
nicholasr74 <nicholasramroop74@gmail.com>
2023-11-15 18:44:12 -0400

    added .java files for grader and unzipper

---
+++ project_marker/src/main/java/oopsy_daisy/AssignmentGrader.java
@@ -0,0 +1,5 @@
+package oopsy_daisy;
+
+public class AssignmentGrader {
+    //Code for Assignment Grader section
+}
---
+++ project_marker/src/main/java/oopsy_daisy/UnZipper.java
@@ -0,0 +1,5 @@
+package oopsy_daisy;
+
+public class UnZipper {
+    //Code for zip file opener
+}
