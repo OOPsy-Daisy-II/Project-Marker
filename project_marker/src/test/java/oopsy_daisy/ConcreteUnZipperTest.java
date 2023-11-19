8ec89ad30f89e643cd9a1e1e5aba6ecd4e1ad52d
J-elcock <122068125+J-elcock@users.noreply.github.com>
2023-11-19 22:08:04 +0000

    added tests to ConcreteUnZipperTest

--- project_marker/src/test/java/oopsy_daisy/ConcreteUnZipperTest.java
+++ project_marker/src/test/java/oopsy_daisy/ConcreteUnZipperTest.java
@@ -1,24 +1,40 @@
 package oopsy_daisy;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertNotNull; 
-import oopsy_daisy.ConcreteUnZipper;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.junit.Before; 
 
-import java.io.File;  
-import java.io.FileNotFoundException;  
-import java.util.Scanner;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
 
 public class ConcreteUnZipperTest{
 
-    ConcreteUnZipper unZipper;
+    public ConcreteUnZipper unZipper;
+    String outputFolder;
+    String zipFile;
+    File destDir;
+    ZipInputStream zis;
+    ZipEntry zipEntry;
+
 
     @Before
-    public void setUp() {
+    void setUp() {
+        destDir = new File(outputFolder + "StudentAssignment");
         unZipper = new ConcreteUnZipper();
-        // File destDir = new File("//workspaces//Project-Marker//project_marker//src");
+        outputFolder = "//workspaces//Project-Marker//project_marker";
+        zipFile = "//workspaces//Project-Marker//project_marker//StudentAssignments.zip";
+
+        try{
+        zis = new ZipInputStream(new FileInputStream(zipFile));
+        zipEntry = zis.getNextEntry();
+        }catch(Exception e){
+
+        }
     }
     
     @Test
@@ -37,9 +53,32 @@ public class ConcreteUnZipperTest{
         
     }
 
-    // @Test
-    // public void testNewFile(){
-    //     assertNotNull(unziper.newFile());
-    // }
+    @Test
+    public void testNewFile(){
+        assertNotNull(unZipper.newFile(destDir, zipEntry));
+    }
+
+    @Test
+    public void testGetFileListInDirectory(){
+        assertFalse(unZipper.getFileListInDirectory(outputFolder + "/StudentAssignments").isEmpty());
+    }
+
+    @Test
+    public void testUnZipList(){
+        ArrayList<String> directoryListing = new ArrayList<String>();
+        directoryListing.add(outputFolder + "/StudentAssignments/818018870-JonathanElcock.zip");
+        
+        assertFalse(unZipper.unZipList(outputFolder, directoryListing).isEmpty());
+    }
+
+    @Test
+    public void testUnzipMasterFile(){
+        assertFalse(unZipper.UnZipMasterFile(zipFile).isEmpty());
+    }
+
+    @Test
+    public void testGetStudentFiles(){
+
+    }
 
 }
