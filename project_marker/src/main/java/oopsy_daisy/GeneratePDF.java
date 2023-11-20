d47b8b57ae85e7b107abc65790df8936844ff16d
nicholasr74 <nicholasramroop74@gmail.com>
2023-11-19 22:01:24 -0400

    committing whatever this is

--- project_marker/src/main/java/oopsy_daisy/GeneratePDF.java
+++ project_marker/src/main/java/oopsy_daisy/GeneratePDF.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.util.stream.Stream;
 
+import com.google.common.collect.Table;
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
@@ -20,10 +21,18 @@ import java.util.List;
 import java.util.ArrayList; 
 
 public class GeneratePDF implements Observer{
-    PdfPTable table;
+    public PdfPTable table;
+    int numberOfColumns = 0; 
+    
 
     public GeneratePDF(String specPath){
+
+        CreatePDF(specPath);   
+    }
+
+    public void CreatePDF(String specPath){
         Document document = new Document();
+        
         document.setPageSize(PageSize.A4.rotate());
         
         ExamineSpec examiner = new ExamineSpec(specPath);
@@ -47,12 +56,14 @@ public class GeneratePDF implements Observer{
             Paragraph newLine = new Paragraph("\n"); // You can customize the content within the paragraph
             document.add(newLine);
 
-            int numberOfColumns = columnCount +3;
-             table = new PdfPTable (numberOfColumns);
-             table.setWidthPercentage(100);
+            numberOfColumns = columnCount +3;
+
+            table = createTable(numberOfColumns);
+            table.setWidthPercentage(100);
+             
+
 
              table.addCell(new PdfPCell(new Paragraph("StudentID")));
-            
             
              for (int i =0; i<columnCount; i++){
                 PdfPCell cell = new PdfPCell(new Paragraph(TestFileList.get(i)));
@@ -62,38 +73,71 @@ public class GeneratePDF implements Observer{
             table.addCell(new PdfPCell(new Paragraph("Total")));
             table.addCell(new PdfPCell(new Paragraph("Comments")));
 
+            //test to show that the PDF is being written to
+            ArrayList<Integer> marks = new ArrayList<>();
+            marks.add(12);
+            marks.add(13);
+            marks.add(41);
+            marks.add(32);
+
+            addRows(table, "specPath", marks);
+             
             document.add(table);
 
 
-        document.close();
+         document.close();
         }
          catch (DocumentException e){
             System.out.println("Error creating pdf");
         }
     }
 
+    private static PdfPTable createTable(int columnCount) {
+        return new PdfPTable(columnCount);
+    }
     
-    private void addRows(String ID, ArrayList<Integer> marks ) {
+
+
+    public void addRows(PdfPTable table, String ID, ArrayList<Integer> marks ) {
         int numColumns = marks.size();
-        System.out.println("MUMCOLUMNS: " + numColumns);
-        table.addCell(ID);
+        System.out.println("Adding Rows to table");
+
+
+        table.addCell(new PdfPCell(new Paragraph(ID)));
+
         int total = 0;
         for (int i =0; i<numColumns; i++){
-                PdfPCell cell = new PdfPCell(new Paragraph(marks.get(i)));
+                PdfPCell cell = new PdfPCell(new Paragraph(String.valueOf(marks.get(i))));
                 total +=marks.get(i);
                 System.out.println("TOTAL "+ total);
                 table.addCell(cell);
              }
             
-        table.addCell(new PdfPCell(new Paragraph(total)));
+        table.addCell(new PdfPCell(new Paragraph(String.valueOf(total))));
         table.addCell(new PdfPCell(new Paragraph("Comments")));
 
 
-        // table.addCell(mark);
+        
     }
 
 
     public void update(){
+        Document temp = new Document();
+        temp.setPageSize(PageSize.A4.rotate());
+
+        try{
+            try{
+                PdfWriter.getInstance(temp, new FileOutputStream("temp.pdf"));
+            } catch (FileNotFoundException e){
+                System.out.println("file not found"); 
+            }
+            
+            temp.open();
+
+            PdfPTable newtable = createTable(numberOfColumns);
+            table.setWidthPercentage(100);
+
+
         ArrayList<Integer> marks = new ArrayList<>(); 
         String studentID = "";
         String comment = "";
@@ -105,8 +149,13 @@ public class GeneratePDF implements Observer{
         marks.add(13);
         marks.add(41);
         marks.add(32);
+
+        addRows(newtable, "816031079", marks); 
+
+        temp.add(newtable);
+        temp.close();
+
         
-        addRows("816031079", marks); 
 
 
         System.out.println("the pdf observer was triggered"); 
